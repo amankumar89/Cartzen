72dd50fbd124448aef5dddaf3488da6b7a1e2c77
amankumar89 <amankumaroo784@gmail.com>
2026-01-01 12:18:50 +0530

    feat: update client structure and add routing
    - Changed the title of the application to "Cartzen - Product Store".
    - Renamed the client package to "cartzen-client".
    - Added new dependencies for Clerk authentication, Tailwind CSS, and DaisyUI.
    - Implemented routing in the App component with pages for Home, Products, Profile, Create Product, and Edit Product.
    - Created new page components: HomePage, ProductsPage, ProfilePage, ProductCreatePage, and EditProductPage.
    - Configured Tailwind CSS and DaisyUI in the CSS file.
    - Updated main entry point to include ClerkProvider and BrowserRouter for routing and authentication.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProductCreatePage from "./pages/ProductCreatePage";
+import EditProductPage from "./pages/EditProductPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen bg-base-100">
+      <div>Navbar</div>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create" element={<ProductCreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
