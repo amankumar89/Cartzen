aa6fbc9a5214f57ef7dc81a8d56ea5aa89907471
amankumar89 <amankumaroo784@gmail.com>
2026-01-10 10:27:56 +0530

    feat: initialize project with package.json and client package-lock.json
    - Added package.json to define project metadata, scripts, and dependencies for the Cartzen application.
    - Introduced package-lock.json for the client to manage dependencies and ensure consistent installations.
    - Removed outdated GitHub Actions workflow and client bun.lock file to streamline the build process.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express, { type Request, type Response } from "express";
 import cors from "cors";
 import { ENV } from "./config/env";
 import { clerkMiddleware } from "@clerk/express";
-// import path from "path";
+import path from "path";
 
 // routes
 import usersRoutes from "./routes/usersRoute";
@@ -16,7 +16,7 @@ app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_: Request, res: Response) => {
+app.get("/api/health", (_: Request, res: Response) => {
   return res.status(200).json({
     message:
       "Welcome to Cartzen API - Powered by PostgreSQL, Drizzle ORM & Clerk Authentication",
@@ -32,17 +32,17 @@ app.use("/api/users", usersRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/comments", commentsRoutes);
 
-// if (ENV.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
 
-//   // serve static files from frontend/dist
-//   app.use(express.static(path.join(__dirname, "../client/dist")));
+  // serve static files from frontend/dist
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-//   // handle SPA routing - send all non-API routes to index.html - react app
-//   app.get("/{*any}", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-//   });
-// }
+  // handle SPA routing - send all non-API routes to index.html - react app
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
 if (ENV.NODE_ENV === "development") {
   app.listen(ENV.PORT, (error: Error | undefined) => {
